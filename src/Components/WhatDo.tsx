809a01c4baad4ce75e29e9c3a3e69c4c932d1aa5
ali abdullah <ali.37803990@gmail.com>
2025-03-17 00:52:55 +0500

    seecond commit

---
+++ src/Components/WhatDo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function WhatDo() {
+  return (
+    <div className='bg-white h-screen flex items-center justify-center'>
+        <div className='text-center'>
+
+        </div>
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default WhatDo
